c7860fae3854f72c5bd32d3306cb94c50307b768
sourav paul <souravpaul@souravs-MacBook-Air-2.local>
2025-02-22 20:56:32 +0530

    update initial changes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Page = () => {
   return (
     <div>
-      <h1 className='h1-bold text-center p-10'>Home page content</h1>
+      <h1 className='h1-bold text-center p-10'>Home page content new</h1>
     </div>
   )
 }
